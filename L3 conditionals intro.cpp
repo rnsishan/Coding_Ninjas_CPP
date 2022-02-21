a30e197c920e3ff74203827ac5d45b7181cd72e8
rnsishan <96770975+rnsishan@users.noreply.github.com>
2022-02-21 23:38:21 +0530

    Create L3 conditionals intro.cpp

---
+++ L3 conditionals intro.cpp
@@ -0,0 +1,20 @@
+//conditionals
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    if(a==b) cout<<"equal"<<endl;
+    else cout<<"unequal";
+
+    }
+}
+    
+}
+//even odd also made in lecture
+    return 0;
+}
