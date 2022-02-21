7e1c6afb74164aeba6d45c1e74ddb524216ded44
rnsishan <96770975+rnsishan@users.noreply.github.com>
2022-02-21 23:23:18 +0530

    Create taking input.cpp

---
+++ taking input.cpp
@@ -0,0 +1,28 @@
+//taking input
+
+
+#include <iostream>
+
+using namespace std;
+
+
+//In C++, all variables contain garbage values before their initialisation.
+
+int main()
+{
+    
+    int a;
+    int b;
+    
+    //cin>>a;
+    //cin>>b;
+    
+    cin>>a>>b;
+    
+    int c=a+b;
+    cout<<c;
+
+    return 0;
+}
+
+
