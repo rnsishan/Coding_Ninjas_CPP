987edc4cb8583d667ecb067ed9e08c7f24f785ba
rnsishan <96770975+rnsishan@users.noreply.github.com>
2022-02-21 23:48:54 +0530

    Create 	L6 increment decrement op.cpp

---
+++ L6 increment decrement op.cpp
@@ -0,0 +1,29 @@
+//L6 increment decrement op
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{  int a=5;
+    //a=a+1 == a++;
+    //a++ //post increment 
+    //++a //pre decrement 
+    cout<<++a<<endl;
+     //a=5;
+    cout<<a++<<endl;
+
+ 
+    cout<<a--<<endl;
+     //a=5;
+    cout<<--a<<endl;
+    
+   // a=a+3 == a+=3;
+    
+    // a+b*c  
+    int b= 10*5/9;
+    cout<<b<<endl;
+    
+    
+    return 0;
+}
