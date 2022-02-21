db1fb60cd4ee2c2bbed9fdac92aed36bf872a79a
rnsishan <96770975+rnsishan@users.noreply.github.com>
2022-02-21 23:22:48 +0530

    Create variables and datatypes.cpp

---
+++ variables and datatypes.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=10;
+    int b=15;
+    int c=a+b;
+
+    cout<<c<<endl;
+    
+    char d='x';     //char 1 byte character single quote
+     cout<< d<<endl;
+     
+     
+     //for string double quote if you directly printing it
+     //bool true or FALSE
+     
+     bool g= true;
+     cout<< g<<endl;
+     
+     float f=1.23;
+     int size = sizeof(f); //sizeof works here
+     
+     cout<< size<<endl;
+     //float 4 bytes
+     //double 8 bytes
+     
+     
+     
+     
+     
+
+    return 0;
+}
