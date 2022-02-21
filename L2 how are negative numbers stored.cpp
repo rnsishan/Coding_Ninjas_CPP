e65e65677e101bf7236decf6e38c7607f5a6aa21
rnsishan <96770975+rnsishan@users.noreply.github.com>
2022-02-21 23:25:46 +0530

    Create L2 how are negative numbers stored.cpp

---
+++ L2 how are negative numbers stored.cpp
@@ -0,0 +1,20 @@
+//how are negative numbers stored
+//2s compliments are stored
+//char -128 to 127 range 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a= 234354;
+    cout<<a<<endl;
+//experimenting
+unsigned int b=-123;
+    cout<<b;
+    
+        
+
+    return 0;
+}
