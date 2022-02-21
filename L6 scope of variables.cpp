bcfecc7a377b04a006be37b901ece488d4c54bd5
rnsishan <96770975+rnsishan@users.noreply.github.com>
2022-02-21 23:51:39 +0530

    Create L6 scope of variables.cpp

---
+++ L6 scope of variables.cpp
@@ -0,0 +1,14 @@
+//L6 scope of variables
+#include <stdio.h>
+
+int main()
+{
+    
+    //parenthesis matters as anything inside it stays inside
+    //any variable stays within the parenthesis
+    //cant have same name variable in one scope
+    
+    
+    return 0;
+}
+
