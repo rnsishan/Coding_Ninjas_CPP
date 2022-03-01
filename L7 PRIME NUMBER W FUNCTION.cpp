669155554ff321a379d42e17e3fcc80f20a75426
rnsishan <ishankoul02@gmail.com>
2022-03-01 17:53:32 +0530

    Create L7 PRIME NUMBER W FUNCTION.cpp

---
+++ L7 PRIME NUMBER W FUNCTION.cpp
@@ -0,0 +1,27 @@
+//L7 PRIME NUMBER W FUNCTION
+#include <iostream>
+
+using namespace std;
+
+bool isprime(int n){
+    int d=2;
+    while(d<n){
+        if(n%d==0){
+            return false;
+        }
+    d++;
+    }
+    return true;
+}
+
+int main()
+{   int n;
+cin>>n;
+
+    for(int x=2;x<n;x++){
+       if(isprime(x)){
+           cout<<x<<endl;
+       }
+    cout<<isprime(n)<<endl;
+}
+
