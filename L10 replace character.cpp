2eadc139af7b966adb516aab3b703efca0717a97
rnsishan <ishankoul02@gmail.com>
2022-03-01 18:51:32 +0530

    Create L10 replace character.cpp

---
+++ L10 replace character.cpp
@@ -0,0 +1,33 @@
+/*
+replace character
+Given an input string S and two characters c1 and c2, you need to replace every occurrence of character c1 with character c2 in the given string.
+
+*/
+
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+void replaceCharacter(char input[], char c1, char c2) {
+    // Write your code here
+    
+    int i=0,count=0;
+    for(i=0;input[i]!='\0';i++){
+    count++;
+    
+    }
+    for(i=0;i<count;i++){
+        if(input[i]==c1){
+            input[i]=c2;
+        }
+    }
+}
+
+int main() {
+    char input[1000000];
+    char c1, c2;
+    cin >> input;
+    cin >> c1 >> c2;
+    replaceCharacter(input, c1, c2);
+    cout << input << endl;
+}
