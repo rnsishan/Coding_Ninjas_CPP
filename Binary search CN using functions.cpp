da28436b57c874df2695579a837e9ec332e73232
rnsishan <ishankoul02@gmail.com>
2022-03-01 18:41:37 +0530

    Create Binary search CN using functions.cpp

---
+++ Binary search CN using functions.cpp
@@ -0,0 +1,51 @@
+//binary search
+#include <iostream>
+
+using namespace std;
+
+int binarySearch(int *input, int n, int val)
+{
+    //Write your code here
+    int mid,start=0,end=n-1;
+    
+    while(start<=end){
+    mid=start+(end-start)/2;
+        
+    if (input[mid]==val)
+        return mid;
+    
+    if (val<input[mid])
+        end=mid-1;
+    else
+        start=mid+1;
+    }    
+    return -1;
+    
+}
+
+int main()
+{
+    int size;
+	cin >> size;
+	int *input = new int[size];
+
+	for(int i = 0; i < size; ++i)
+	{
+		cin >> input[i];
+	}
+	
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int val;
+		cin >> val;
+		cout << binarySearch(input, size, val) << endl;
+	}
+	
+	delete [] input;
+	return 0;
+
+   
+}
