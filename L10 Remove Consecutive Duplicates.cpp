b5b6782bd7e3f3c9d6fb80260c95d4b301cd88ee
rnsishan <ishankoul02@gmail.com>
2022-03-01 18:55:03 +0530

    Create L10 Remove Consecutive Duplicates.cpp

---
+++ L10 Remove Consecutive Duplicates.cpp
@@ -0,0 +1,35 @@
+/*Remove Consecutive Duplicates
+For a given string(str), remove all the consecutive duplicate characters.
+*/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void removeConsecutiveDuplicates(char input[]) {
+    // Write your code here
+    int i = 0;
+  int j = 1;
+  int k = 0;
+  while(i<strlen(input)){
+    input[k]=input[i];
+    while(input[i]==input[j]){
+      i++;
+      j++;
+    }
+    i++;
+    j++;
+    k++;
+  }
+  input[k]='\0';
+}
+
+
+
+int main() {
+    int size = 1e6;
+    char str[size];
+    cin >> str;
+    removeConsecutiveDuplicates(str);
+    cout << str;
+}
