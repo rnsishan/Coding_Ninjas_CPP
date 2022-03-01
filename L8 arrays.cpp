e396b28d78a2d4a9146423997d7215c0c5d0a62b
rnsishan <ishankoul02@gmail.com>
2022-03-01 17:54:46 +0530

    Create L8 arrays.cpp

---
+++ L8 arrays.cpp
@@ -0,0 +1,38 @@
+//L8 ARRAYS
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int b[10];
+    b[9]=11;
+    cout<<b[9]<<endl;
+
+    //simply describes how to make an array
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
